0fc13287a55b485a24d319191591e5ea741eef7f
nefelinikiforou <p15niki@ionio.gr>
2018-03-10 19:25:11 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -295,7 +295,7 @@ function moveToAmount(alpha) {
 		d.x += (centreX - d.x) * (brake + 0.06) * alpha * 1.2;
 		d.y += (centreY - 100 - d.y) * (brake + 0.06) * alpha * 1.2;
 	};
-
+}
 // Collision detection function by m bostock
 function collide(alpha) {
   var quadtree = d3.geom.quadtree(nodes);
